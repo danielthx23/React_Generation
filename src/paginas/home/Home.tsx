32933d1908f2d3a7c95243a409c2460913dd0cb5
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-06-21 13:27:39 -0300

    Primeira Parte do React - Cookbook 4

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">❤️ Teletubbies ❤️</h1>
+            <img className="imagem" src="https://laparola.com.br/wp-content/uploads/2015/03/teletubbies-preto-e-branco.jpg" alt="Tela legal" />
+        </>
+    )
+}
+
+export default Home

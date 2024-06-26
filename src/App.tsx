39b5632128d339d528467556e212cbdf0b796fd6
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-06-25 21:33:45 -0300

    Navbar, footer e home editados do blogpessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 import Home from './paginas/home/Home';
-import Login from './paginas/login/Login.tsx';
+import Navbar from './components/navbar/NavBar';
+import Footer from './components/footer/Footer';
 
 function App() {
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Home />
+      <Footer />
     </>
-  )
+  );
 }
-
-export default App
+export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,95 +1,27 @@
-import React, { } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import React from 'react';
+import './Home.css';
+
 
 function Home() {
-    const navigate = useNavigate();
     return (
         <>
-            <div>
-                <div className='p-10 flex flex-col items-center gap-5 bg-slate-200'>
-                    <h2 className="text-slate-900 text-5xl m-4">Home</h2>
-                    <div>
-                        <button type='submit'
-                            className='hover:underline mx-4 bg-black text-white rounded px-8 py-4'
-                            onClick={() => { navigate('/login') }}>
-                            Login useNavigate
-                        </button>
-                        <Link to='/login' className='hover:underline mx-4 bg-black text-white rounded px-8 py-4'>Login por Link</Link>
-                    </div>
-                </div>
-                <div className='p-10 text-center flex flex-col gap-5'>
-                    <h3 className='text-4xl'>justify-content alinhamento horizontal</h3>
-                    <h5 className='text-2xl'>justify-start Esquerda</h5>
-                    <div className='flex flex-row justify-start m-3 text-2xl'>
-                        <div className='basis-1/4 bg-red-500 p-2'>01</div>
-                        <div className='basis-1/4 bg-gray-400 p-2'>01</div>
-                    </div>
-                    <h5 className='text-2xl'>justify-end Direita</h5>
-                    <div className='flex flex-row justify-end m-3 text-2xl'>
-                        <div className='basis-1/4 bg-red-500 p-2'>01</div>
-                        <div className='basis-1/4 bg-gray-400 p-2'>01</div>
-                    </div>
-                    <h5 className='text-2xl'>justify-center Centro</h5>
-                    <div className='flex flex-row justify-center m-3 text-2xl'>
-                        <div className='basis-1/4 bg-red-500 p-2'>01</div>
-                        <div className='basis-1/4 bg-gray-400 p-2'>01</div>
-                    </div>
-                    <h5 className='text-2xl'>justify-between Extremidades</h5>
-                    <div className='flex flex-row justify-between m-3 text-2xl'>
-                        <div className='basis-1/4 bg-red-500 p-2'>01</div>
-                        <div className='basis-1/4 bg-gray-400 p-2'>01</div>
-                    </div>
-                    <h5 className='text-2xl'>justify-around Extremidades</h5>
-                    <div className='flex flex-row justify-around m-3 text-2xl'>
-                        <div className='basis-1/4 bg-red-500 p-2'>01</div>
-                        <div className='basis-1/4 bg-gray-400 p-2'>01</div>
-                    </div>
+            <div className="bg-teal-200 flex justify-center h-screen">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+                        <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
 
-                    <div className='p-10 text-center flex flex-col gap-5'>
-                        <h3 className='text-4xl'>justify-content alinhamento vertical</h3>
-                        <div className='w-auto h-60 bg-gray-400 flex justify-center items-start m-3 text-2xl'>
-                            <div className='basis-1/4 bg-red-500 p-2'>justify-center items-start alinhamento horizontal centro/vertical topo</div>
-                        </div>
-                        <div className='w-auto h-60 bg-gray-400 flex justify-center items-center m-3 text-2xl'>
-                            <div className='basis-1/4 bg-red-500 p-2'>justify-center items-start alinhamento horizontal centro/vertical centro</div>
-                        </div>
-                        <div className='w-auto h-60 bg-gray-400 flex justify-center items-end m-3 text-2xl'>
-                            <div className='basis-1/4 bg-red-500 p-2'>justify-center items-start alinhamento horizontal centro/vertical base</div>
+                        <div className="flex justify-around gap-4">
+
+                            <button className='rounded bg-white text-teal-800 py-2 px-4'>Ver postagens</button>
                         </div>
                     </div>
 
-                    <div className='p-10 text-center flex flex-col gap-5'>
-                        <h3 className='text-4xl'>grids</h3>
-                        <div className='grid grid-cols-12 gap-2 m-4 text-2xl'>
-                            <div className='col-span-6 bg-green-300 p-2'> container 1 </div>
-                            <div className='col-span-6 bg-yellow-200 p-2'> container 2 </div>
-                        </div>
-                        <div className="grid grid-cols-12 gap-2 m-4 text-2xl">
-                            <div className='col-span-8 bg-green-300 p-2'> container 1</div>
-                            <div className='col-span-4 bg-yellow-200 p-2'> container 2</div>
-                        </div>
-                        <div className='grid grid-cols-12 gap-2 m-4 text-2xl'>
-                            <div className='col-span-4 bg-green-300 p-2'>container 1</div>
-                            <div className='col-span-4 bg-yellow-200 p-2'>container 2</div>
-                            <div className='col-span-4 bg-red-500 p-2'>container 3</div>
-                        </div>
-                        <div className='grid grid-cols-12 gap-2 m-4 text-2xl'>
-                            <div className='col-span-3 bg-green-300 p-2'>container 1</div>
-                            <div className='col-span-3 bg-yellow-200 p-2'>container 2</div>
-                            <div className='col-span-3 bg-red-500 p-2'>container 3</div>
-                            <div className='col-span-3 bg-blue-500 p-2'>container 4</div>
-                        </div>
-                        <div className='grid grid-cols-12 gap-2 m-4 text-2xl'>
-                            <div className='col-span-4 bg-green-300 p-2'>container 1</div>
-                            <div className='col-span-4 bg-yellow-200 p-2'>container 2</div>
-                            <div className='col-span-2 bg-red-500 p-2'>container 3</div>
-                            <div className='col-span-2 bg-blue-500 p-2'>container 4</div>
-                        </div>
-                    </div>
                 </div>
             </div>
+
         </>
-    )
+    );
 }
 
-export default Home
+export default Home;
